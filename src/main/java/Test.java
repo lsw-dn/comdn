4ad5730116c9c6bbaa6bd24f093f6944cd97c7ea
lsw-dn <1656569657@qq.com>
2019-01-13 22:13:41 +0800

    v1

---
+++ src/main/java/Test.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.*;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        executorService.submit()
+
+        Executors.newScheduledThreadPool()
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1,
+                3,20, TimeUnit.DAYS,
+                new LinkedBlockingDeque<Runnable>(30),new CustomRejectedExecutionHandler());
+
+        threadPoolExecutor.getRejectedExecutionHandler(new RejectedExecutionHandler(){
+
+            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+
+            }
+        });
+    }
+}
